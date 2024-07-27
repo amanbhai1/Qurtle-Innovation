7c214bb6f7facf0af5f13799be29e16003c58ce8
amanbhai1 <49727102+amanbhai1@users.noreply.github.com>
2024-07-27 23:27:25 +0530

    services page done

--- script.js
+++ script.js
@@ -36,11 +36,19 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.pathname.endsWith('services.html')) {
+        document.body.style.backgroundImage = "url('/Assets/interior-header-image.jpg')";
+        document.body.style.backgroundSize ="180%";
+        
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
     if (window.location.pathname.endsWith('about.html')) {
         document.body.style.backgroundImage = "url('/Assets/interior-header-image.jpg')";
         
         if (window.innerWidth <= 855) {
-            document.body.style.backgroundSize = 'contain';
+            document.body.style.backgroundSize = '310';
         }
         if (window.innerWidth <= 560) {
             document.body.style.backgroundSize = '600%';
@@ -48,3 +56,54 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.pathname.endsWith('services.html')) {
+        var missionElements = document.querySelectorAll('.our-mission');
+        var missionElements2 = document.querySelectorAll('.our-mission-data ul li');
+        var missionElements3 = document.querySelectorAll('.our-mission h1');
+        missionElements.forEach(function(element) {
+            element.style.fontSize = '9px'; 
+            if (window.innerWidth <= 855) {
+                element.style.fontSize = '9px';
+            }
+            if (window.innerWidth <= 560) {
+                element.style.fontSize = '9px';
+            }
+        });
+
+        missionElements2.forEach(function(element) {
+            element.style.fontSize = '15px'; 
+            element.style.margin = '10px 130px'
+            if (window.innerWidth <= 855) {
+                element.style.fontSize = '15px';
+                element.style.margin = '10px 60px'
+            }
+            if (window.innerWidth <= 560) {
+                element.style.fontSize = '15px';
+            }
+        });
+
+        missionElements3.forEach(function(element) {
+            element.style.fontSize = '9px'; 
+            if (window.innerWidth <= 855) {
+                element.style.fontSize = '15px';
+            }
+            if (window.innerWidth <= 560) {
+                element.style.fontSize = '15px';
+            }
+        });
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.location.pathname.endsWith('services.html')) {
+        document.body.style.backgroundImage = "url('/Assets/interior-header-image.jpg')";
+        
+        if (window.innerWidth <= 855) {
+            document.body.style.backgroundSize = '310%';
+        }
+        if (window.innerWidth <= 560) {
+            document.body.style.backgroundSize = '600%';
+        }
+    }
+});
