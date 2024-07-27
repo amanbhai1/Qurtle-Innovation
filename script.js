57ff0f284ae3abf2a07f3a6f6ac42fa2ee546a0e
amanbhai1 <49727102+amanbhai1@users.noreply.github.com>
2024-07-27 13:47:24 +0530

    New update

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+function openSidebar() {
+    document.getElementById("navbar").classList.add("active");
+}
+
+function closeSidebar() {
+    document.getElementById("navbar").classList.remove("active");
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('Blog');
-    
     button.addEventListener('click', function() {
         window.location.href = 'blog.html';
     });
