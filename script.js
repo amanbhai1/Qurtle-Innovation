9594074c34dd3aab40dc7e00d865b3c80eced208
amanbhai1 <49727102+amanbhai1@users.noreply.github.com>
2024-07-28 14:00:10 +0530

    commit

--- script.js
+++ script.js
@@ -59,7 +59,9 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     if (window.location.pathname.endsWith('services.html')) {
         var missionElements2 = document.querySelectorAll('.our-mission-data ul li');
+        var missionElements5 = document.querySelectorAll('.our-mission-data p');
         var missionElements3 = document.querySelectorAll('.our-mission h1');
+        var missionElements4 = document.querySelectorAll('.active1');
 
         missionElements2.forEach(function(element) {
             element.style.fontSize = '15px'; 
@@ -74,12 +76,33 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
         missionElements3.forEach(function(element) {
-            element.style.fontSize = '18px'; 
+            element.style.fontSize = '25px'; 
             if (window.innerWidth <= 855) {
-                element.style.fontSize = '12px';
+                element.style.fontSize = '25px';
             }
             if (window.innerWidth <= 560) {
-                element.style.fontSize = '15px';
+                element.style.fontSize = '18px';
+            }
+        });
+
+        missionElements5.forEach(function(element) {
+            element.style.fontSize = '25px'; 
+            if (window.innerWidth <= 855) {
+                element.style.fontSize = '25px';
+            }
+            if (window.innerWidth <= 560) {
+                element.style.fontSize = '16px';
+            }
+        });
+
+        missionElements4.forEach(function(element) {
+            element.style.width = '25%'; 
+            if (window.innerWidth <= 855) {
+                element.style.width = '25px';
+
+            }
+            if (window.innerWidth <= 560) {
+                element.style.width= '60%';
             }
         });
     }
